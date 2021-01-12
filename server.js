11818b61d7bbf605239bf9aabef423e21fec4934
Kaloyan Marinov <kallo.marinov@gmail.com>
2021-01-12 20:11:29 +0200

    create a _proxy_ to a database

--- server.js
+++ server.js
@@ -5,6 +5,24 @@ const bodyParser = require("koa-body");
 const app = new Koa();
 const router = new Router();
 
+// Pretend the following list is a database.
+let users = [
+  [
+    {
+      name: "Jacob",
+      email: "info@geeklaunch.net",
+    },
+    {
+      name: "Kenny",
+      email: "kenny@example.net",
+    },
+    {
+      name: "Joe",
+      email: "joe@example.org",
+    },
+  ],
+];
+
 app.use(router.allowedMethods()).use(router.routes()).use(bodyParser());
 
 app.listen(3000);
