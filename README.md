a7a38c952c23dc3c042e717a8c92af15db81ea0b
Kaloyan Marinov <kallo.marinov@gmail.com>
2021-01-12 20:11:29 +0200

    implement a handler for GET requests to /user/:id

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
-To start the server, issue
+To start the server, open a terminal session and issue
 
 ```
 $ node server.js
+```
+
+If the server is running and you open a second session terminal session, you can issue each of the following in the second session:
+
+```
+$ curl localhost:3000/user/0
+{"name":"Jacob","email":"info@geeklaunch.net"}
+$ curl localhost:3000/user/1
+{"name":"Kenny","email":"kenny@example.net"}
+$ curl localhost:3000/user/2
+{"name":"Joe","email":"joe@example.org"}
 ```
--- server.js
+++ server.js
@@ -7,21 +7,23 @@ const router = new Router();
 
 // Pretend the following list is a database.
 let users = [
-  [
-    {
-      name: "Jacob",
-      email: "info@geeklaunch.net",
-    },
-    {
-      name: "Kenny",
-      email: "kenny@example.net",
-    },
-    {
-      name: "Joe",
-      email: "joe@example.org",
-    },
-  ],
+  {
+    name: "Jacob",
+    email: "info@geeklaunch.net",
+  },
+  {
+    name: "Kenny",
+    email: "kenny@example.net",
+  },
+  {
+    name: "Joe",
+    email: "joe@example.org",
+  },
 ];
+
+router.get("/user/:id", async (ctx) => {
+  ctx.body = users[ctx.params.id];
+});
 
 app.use(router.allowedMethods()).use(router.routes()).use(bodyParser());
 
